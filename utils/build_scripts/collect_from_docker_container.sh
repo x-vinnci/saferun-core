f3325f9ec8790f49bbd06a7533ecd5429b6613f5
Jason Rhinelander <jason@imaginary.ca>
2021-03-26 16:10:42 -0300

    Clean up unused/unmaintained contrib and utils
    Everything removed here is unsupported and doesn't work (leftover stuff
    from Monero).

--- utils/build_scripts/collect_from_docker_container.sh
+++
@@ -1,4 +0,0 @@
-set -ex && mkdir -p build/release/bin
-set -ex && docker create --name loki-daemon-container loki-daemon-image
-set -ex && docker cp loki-daemon-container:/usr/local/bin/ build/release/
-set -ex && docker rm loki-daemon-container
