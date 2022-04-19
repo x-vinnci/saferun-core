f360775cfe4bb8b6676ee1370300a5beae92c037
Jason Rhinelander <jason@imaginary.ca>
2022-04-19 19:09:31 -0300

    Reset devnet
    Rewrite one of the devnet hardforks to revert the chain; it has stalled
    without enough active nodes to do pulse, so tweak one of the hardfork
    heights to reset things.

--- src/cryptonote_basic/hardfork.cpp
+++ src/cryptonote_basic/hardfork.cpp
@@ -75,7 +75,7 @@ static constexpr std::array devnet_hard_forks =
   hard_fork{ 12, 0,     3,  1599848400 },
   hard_fork{ 13, 0,     4,  1599848400 }, 
   hard_fork{ 15, 0,     5,  1599848400 },
-  hard_fork{ 16, 0,    99,  1599848400 },
+  hard_fork{ 16, 0,   100,  1599848400 },
   hard_fork{ 17, 0,   151,  1599848400 },
   hard_fork{ 18, 0,   152,  1599848400 },
   hard_fork{ 19, 0,   153,  1599848400 },
