a86995c0572b528fba87029262924ff777324c37
Jason Rhinelander <jason@imaginary.ca>
2022-09-26 18:47:38 -0300

    sqlite3: update to latest; run autoreconf before configure
    At some point the default automake version changed to 1.16, which bionic
    doesn't support, so run autoreconf first to make sure it'll work.

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -27,11 +27,11 @@ set(READLINE_SOURCE readline-${READLINE_VERSION}.tar.gz)
 set(READLINE_HASH SHA512=27790d0461da3093a7fee6e89a51dcab5dc61928ec42e9228ab36493b17220641d5e481ea3d8fee5ee0044c70bf960f55c7d3f1a704cf6b9c42e5c269b797e00
     CACHE STRING "readline source hash")
 
-set(SQLITE3_VERSION 3390200 CACHE STRING "sqlite3 version")
+set(SQLITE3_VERSION 3390300 CACHE STRING "sqlite3 version")
 set(SQLITE3_MIRROR ${LOCAL_MIRROR} https://www.sqlite.org/2022
     CACHE STRING "sqlite3 download mirror(s)")
 set(SQLITE3_SOURCE sqlite-autoconf-${SQLITE3_VERSION}.tar.gz)
-set(SQLITE3_HASH SHA512=c16b50ade3c182d5473014ac0a51e2bb8a5cfc46e532c2bda77ae4d530336e2b57aa4f12dccb6aa2148d60e9289305bf20842ac95dc52f2d31df8eb5f0599de6
+set(SQLITE3_HASH SHA512=f5f1f275ca7def6e1971d0152852468b89f2759d6fc2dda42b6e47646d65c7d8c454a9accd4ac900e171908114a9164c722314b90fb2ca422b99603158f9ce3e
     CACHE STRING "sqlite3 source hash")
 
 if(SQLITE3_VERSION MATCHES "^([0-9]+)(0([0-9])|([1-9][0-9]))(0([0-9])|([1-9][0-9]))[0-9][0-9]$")
@@ -346,6 +346,8 @@ set(Boost_VERSION ${BOOST_VERSION})
 
 build_external(sqlite3
   BUILD_COMMAND true
+  CONFIGURE_COMMAND autoreconf -ivf && ./configure ${cross_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
+    "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}" "CXXFLAGS=${deps_CXXFLAGS}" ${cross_extra}
   INSTALL_COMMAND make install-includeHEADERS install-libLTLIBRARIES)
 add_static_target( SQLite::SQLite3 sqlite3_external libsqlite3.a)
 
