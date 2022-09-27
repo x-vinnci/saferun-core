dbf6c2dd4513c448b16f30b6562bdc7a2a5fee32
Jason Rhinelander <jason@imaginary.ca>
2022-09-27 17:27:18 -0300

    Make ghc::filesystem fmt-formattable

--- src/common/fs.h
+++ src/common/fs.h
@@ -20,6 +20,16 @@ namespace fs {
 #else
 
 #include <ghc/filesystem.hpp>
+#include "formattable.h"
+
 namespace fs = ghc::filesystem;
+
+namespace formattable {
+  template <> inline constexpr bool via_to_string<ghc::filesystem::path> = true;
+
+  std::string to_string(const ghc::filesystem::path& path) {
+    return path.string();
+  }
+}
 
 #endif
